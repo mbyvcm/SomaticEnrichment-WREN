d439356dc584cad7658b219321562b1685fad4b8
Christopher Medway <christophermedway@gmail.com>
2020-09-01 18:34:26 +0100

    remove absolute paths

--- SomaticEnrichmentLib-2.0.0/manta.sh
+++ SomaticEnrichmentLib-2.0.0/manta.sh
@@ -7,28 +7,32 @@ set -euo pipefail
 seqId=$1
 sampleId=$2
 panel=$3
-primaryBed=$4
+vendorPrimaryBed=$4
 
-if [ -d /data/results/$seqId/$panel/$sampleId/MANTA ]
+if [ -d MANTA ]
 then
-    rm -r /data/results/$seqId/$panel/$sampleId/MANTA
+    rm -r MANTA
 fi
 
 
-mkdir /data/results/$seqId/$panel/$sampleId/MANTA/
+mkdir MANTA
 
-source /home/transfer/miniconda3/bin/activate manta
+set +u
+source activate manta
+set -u
 
-cat $primaryBed | bgzip > /data/results/$seqId/$panel/$sampleId/MANTA/callRegions.bed.gz
-tabix -p bed  /data/results/$seqId/$panel/$sampleId/MANTA/callRegions.bed.gz
+cat $vendorPrimaryBed | bgzip > MANTA/callRegions.bed.gz
+tabix -p bed MANTA/callRegions.bed.gz
 
 configManta.py \
-    --tumorBam /data/results/$seqId/$panel/$sampleId/"$seqId"_"$sampleId".bam \
-    --referenceFasta /data/db/human/gatk/2.8/b37/human_g1k_v37.fasta \
-    --runDir  /data/results/$seqId/$panel/$sampleId/MANTA/ \
+    --tumorBam "$seqId"_"$sampleId".bam \
+    --referenceFasta /home/transfer/resources/human/gatk/2.8/b37/human_g1k_v37.fasta \
+    --runDir  ./MANTA \
     --exome \
-    --callRegions /data/results/$seqId/$panel/$sampleId/MANTA/callRegions.bed.gz
+    --callRegions ./MANTA/callRegions.bed.gz
 
-/data/results/$seqId/$panel/$sampleId/MANTA/runWorkflow.py --quiet -m local
+./MANTA/runWorkflow.py --quiet -m local
 
-source /home/transfer/miniconda3/bin/deactivate
+set +u
+conda deactivate
+set -u
